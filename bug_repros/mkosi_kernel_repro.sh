76a1a52a6dca25de5fdb304994c0a2ff28f31692
Ryan Rhodes Newton <newton@fb.com>
2025-06-30 07:53:35 -0700

    Add bug repro script

---
+++ bug_repros/mkosi_kernel_repro.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+uname -a
+cat /etc/issue /etc/redhat-release
+set -euo pipefail
+
+if ! [ -d ./mkosi ]; then
+    git clone https://github.com/systemd/mkosi
+fi
+(cd ./mkosi && git checkout 0d1143150835b21c1bfe64428df5f45b558280b1)
+export PATH="$PATH:$PWD/mkosi/bin/"
+
+echo;echo "Mkosi version, running from source:"
+mkosi --version
+
+if ! [ -d ./mkosi-kernel ]; then
+    git clone https://github.com/DaanDeMeyer/mkosi-kernel.git
+fi
+cd ./mkosi-kernel
+git checkout 7beb959e51354077ded4333d2c9951909ea46c75
+
+
+# For devvm:
+export https_proxy=fwdproxy:8080
+
+# As per the README:
+mkosi -f qemu
