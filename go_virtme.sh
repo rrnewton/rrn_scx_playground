632acb34fc1069925db8e478983b50a124d0c08b
Ryan Newton <rrnewton@gmail.com>
2025-07-03 13:17:32 -0400

    Fix to script -- works fine on vanilla ubuntu.

--- go_virtme.sh
+++ go_virtme.sh
@@ -11,9 +11,5 @@ cd "$dir/linux"
 
 set +euo pipefail
 echo "Running slightly patched Virtme..."
-# cat | "$virtme" --kimg arch/x86/boot/bzImage --rw --pwd --memory 1024M --script-sh "echo HelloWorld"
-"$virtme" --kimg arch/x86/boot/bzImage --rw --pwd --memory 1024M --script-exec ./hello.static
-
-# Host File system is read/write, no? I guess that's just the guest root file system.
-# "$virtme" --kimg arch/x86/boot/bzImage --rw --pwd --memory 1024M --script-sh "touch /home/newton/test_touch.txt"
+"$virtme" --kimg arch/x86/boot/bzImage --rw --pwd --memory 1024M --script-exec ../hello.static
 echo "Virtme run complete: $?"
