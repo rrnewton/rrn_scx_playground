54d504305c7844e9af664731ffc6ed008d86ce8a
Ryan Rhodes Newton <newton@fb.com>
2025-07-10 09:27:56 -0700

    debugging a schtest issue

--- go_virtme.sh
+++ go_virtme.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
-# ASSUMES: Kernel and ./hello.static are built already.
-
+# ASSUMES: Kernel is already built.
 set -xeuo pipefail
 
 cd $(dirname $0)
 dir=$(pwd)
 virtme="$dir/virtme-rrn/virtme-run"
 
-cd "$dir/linux"
-
 # make hello.static
 # PROG=../hello.static
 
+cd "$dir/schtest"
+cargo build
+
+cd "$dir/linux"
 make -j16 -C tools/sched_ext
+
 PROG=../guest_script.sh
+# PROG="../schtest/target/debug/schtest"
+# PROG="whoami"
 
 set +euo pipefail
 echo "Running slightly patched Virtme..."
-"$virtme" --kimg arch/x86/boot/bzImage --rw --pwd --memory 1024M --script-exec $PROG
+"$virtme" --kimg arch/x86/boot/bzImage --rw --pwd --cpus 3 --memory 1024M --script-exec $PROG
 echo "Virtme run complete: $?"
--- guest_script.sh
+++ guest_script.sh
@@ -9,19 +9,50 @@ if [ -e /sys/kernel/sched_ext/root/ops ]; then
     cat /sys/kernel/sched_ext/root/ops
 fi
 
-function trial() {
-    ./tools/sched_ext/build/bin/scx_simple &
-    test_pid=$!
 
-    sleep 3
-    kill $test_pid
-    sleep 1
-    cat /sys/kernel/sched_ext/enable_seq
+RED='\033[0;31m'  # Red text
+GREEN='\033[0;32m'  # Green text
+BLUE='\033[0;94m'  # Bright Blue
+RESET='\033[0m'   # Reset to default color
+
+function redcolor() {
+    set +x
+    # Read input line by line and print in color
+    while IFS= read -r line; do
+        echo -e "${RED}${line}${RESET}"
+    done
 }
 
-trial;
-trial;
+function greencolor() {
+    set +x
+    while IFS= read -r line; do
+        echo -e "${GREEN}${line}${RESET}"
+    done
+}
 
-# ./schtest/target/debug/schtest --filter spread_out
+function bluecolor() {
+    set +x
+    while IFS= read -r line; do
+        echo -e "${BLUE}${line}${RESET}"
+    done
+}
+
+function trial() {
+    # tail -f $1 > >(nc -l -p 9977) &
+    ./tools/sched_ext/build/bin/scx_qmap -P > >(greencolor) &
+    test_pid=$!
+    sleep 1
+    cat /sys/kernel/debug/tracing/trace_pipe > >(redcolor) &
+    cat_pid=$!
+
+    ../schtest/target/debug/schtest --filter spread_out 2>&1 | bluecolor
+    kill -SIGINT $test_pid
+    kill $cat_pid
+    echo "SCX invocation count: "$(cat /sys/kernel/sched_ext/enable_seq)
+}
+
+(whoami; cat /proc/self/cgroup; find /sys/fs/cgroup) | greencolor
+trial;
+trial;
 
 echo "Done with script."
