f8c494ad4d031409b2e9b95fe3a4d91254ea7bb8
Ryan Rhodes Newton <newton@fb.com>
2025-07-20 13:47:33 -0700

    debugging

--- guest_script.sh
+++ guest_script.sh
@@ -13,6 +13,7 @@ fi
 RED='\033[0;31m'  # Red text
 GREEN='\033[0;32m'  # Green text
 BLUE='\033[0;94m'  # Bright Blue
+YELLOW='\033[0;33m'  # Yellow text
 RESET='\033[0m'   # Reset to default color
 
 function redcolor() {
@@ -37,13 +38,35 @@ function bluecolor() {
     done
 }
 
+function yellowcolor() {
+    set +x
+    while IFS= read -r line; do
+        echo -e "${YELLOW}${line}${RESET}"
+    done
+}
+
+
+# Takes the color as the first argument
+function colorlines() {
+    set +x
+    # Read input line by line and print in color
+    while IFS= read -r line; do
+        echo -e "${1}${line}${RESET}"
+    done
+}
+
+alias yellowcolor2="colorlines $YELLOW"
+
 function trial() {
-    # tail -f $1 > >(nc -l -p 9977) &
     ./tools/sched_ext/build/bin/scx_qmap -P > >(greencolor) &
     test_pid=$!
     sleep 1
     cat /sys/kernel/debug/tracing/trace_pipe > >(redcolor) &
     cat_pid=$!
+
+    # Second scheduler fails of course, including non-zero exit code.''
+    # ./tools/sched_ext/build/bin/scx_qmap || echo "PROCESS DIED, exit code $?"
+    # ../scx/target/debug/scx_lavd || echo "PROCESS DIED, exit code $?"
 
     ../schtest/target/debug/schtest --filter spread_out 2>&1 | bluecolor
     kill -SIGINT $test_pid
@@ -51,8 +74,9 @@ function trial() {
     echo "SCX invocation count: "$(cat /sys/kernel/sched_ext/enable_seq)
 }
 
-(whoami; cat /proc/self/cgroup; find /sys/fs/cgroup) | greencolor
+(dmesg -W | yellowcolor) &
+# (whoami; cat /proc/self/cgroup; find /sys/fs/cgroup) | greencolor
 trial;
-trial;
+# trial;
 
 echo "Done with script."
