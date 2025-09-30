504d845edd1dc97032238bf42aefa05396e1e0f8
Ryan Newton <rrnewton@gmail.com>
2025-09-29 21:12:22 -0400

    good tip for stress tests

--- go_virtme.sh
+++ go_virtme.sh
@@ -15,6 +15,6 @@ PROG=../guest_script.sh
 
 set +euo pipefail
 echo "Running slightly patched Virtme..."
-"$virtme" --kimg arch/x86/boot/bzImage --rw --pwd --cpus 50 --memory 120G --script-exec $PROG
-# "$virtme" --kimg arch/x86/boot/bzImage --rw --pwd --cpus 3 --memory 2G --script-exec $PROG
+# "$virtme" --kimg arch/x86/boot/bzImage --rw --pwd --cpus 50 --memory 120G --script-exec $PROG
+"$virtme" --kimg arch/x86/boot/bzImage --rw --pwd --cpus 3 --memory 2G --script-exec $PROG
 echo "Virtme run complete: $?"
